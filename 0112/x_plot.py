9aef7e1d0832fb5b7dfaae91ffcfcdc14a366506
KYH04444 <121211187+KYH04444@users.noreply.github.com>
2024-01-15 16:09:07 +0900

    Update x_plot.py

--- 0112/x_plot.py
+++ 0112/x_plot.py
@@ -21,6 +21,10 @@ with open('sc1_robot2_gt.txt', 'r') as file:
 
 esti_x = []
 esti_y = []
+
+_esti_x = []
+_esti_y = []
+
 rosgbag_time = []
 rosgbag_time_plus = []
 
@@ -38,10 +42,10 @@ for line in rostime_plus:
 
 for line in rosbag_x:
     esti_x.append(float(line.strip()))
-
+    _esti_x.append(0)
 for line in rosbag_y:
     esti_y.append(float(line.strip()))
-
+    _esti_y.append(0)
 downsample_ratio = int(9535 / 1127)
 cnt = 0
 print(downsample_ratio)
@@ -65,11 +69,11 @@ min_length = min(len(gt_robot1_x), len(esti_x), len(esti_y))
 
 for i in range(min_length):
     theta_cnt = i * 2 / 1127 * np.pi
-    esti_x[i] = (np.cos(theta_cnt) * esti_x[i] - np.sin(theta_cnt) * esti_y[i])  + gt_robot1_x[i] 
-    esti_y[i] = (np.sin(theta_cnt) * esti_x[i] + np.cos(theta_cnt) * esti_y[i]) + gt_robot1_y[i]  
+    _esti_x[i] = (np.cos(theta_cnt+np.pi/2) * esti_x[i] - np.sin(theta_cnt+np.pi/2) * esti_y[i])  + gt_robot1_x[i] 
+    _esti_y[i] = (np.sin(theta_cnt+np.pi/2) * esti_x[i] + np.cos(theta_cnt+np.pi/2) * esti_y[i]) + gt_robot1_y[i]  
 
 color = 'tab:red'
-plt.plot(rosgbag_time, esti_y, marker='s', linestyle='-', color=color, label='Estimation of Y')
+plt.plot(rosgbag_time, _esti_y, marker='s', linestyle='-', color=color, label='Estimation of Y')
 
 color = 'tab:blue'
 plt.plot(rosgbag_time_plus, gt_robot2_y, marker='s', linestyle='-', color=color, label='GT of Y')
@@ -77,7 +81,7 @@ plt.plot(rosgbag_time_plus, gt_robot2_y, marker='s', linestyle='-', color=color,
 # color = 'tab:green'
 # plt.plot(gt_robot1_x, gt_robot1_y, marker='s', linestyle='-', color=color, label='gt_r1')
 
-plt.title('Estimation')
+plt.title('EKF')
 plt.xlabel('Time')
 plt.ylabel('Estimated Y')
 plt.grid(True)

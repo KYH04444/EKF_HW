f1301378cde0929a4d3f9a885c75791dc0980d1a
KYH04444 <121211187+KYH04444@users.noreply.github.com>
2024-01-19 20:41:18 +0900

    Update EKF_plot.py

--- 0117/sc1_2/EKF_plot.py
+++ 0117/sc1_2/EKF_plot.py
@@ -1,133 +1,71 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math as m
-def EKF():
-    with open('rosbag_x.txt', 'r') as file:
-        rosbag_x = file.readlines()
+def linearplot():
+    with open('linear_vel_robot2.txt', 'r') as file:
+        lin_vel_r2 = file.readlines()
 
-    with open('rosbag_y.txt', 'r') as file:
-        rosbag_y = file.readlines()
+    with open('angular_vel_robot2.txt', 'r') as file:
+        ang_vel_r2 = file.readlines()
 
-    with open('theta_i.txt', 'r') as file:
-        theta_i = file.readlines()
+    with open('bag_vj.txt', 'r') as file:
+        bag_vj = file.readlines()
 
-    # with open('rosbag_time.txt', 'r') as file:
-    #     rostime = file.readlines()
+    with open('bag_wj.txt', 'r') as file:
+        bag_wj = file.readlines()
 
-    with open('sc1_robot1_gt.txt', 'r') as file:
-        robot1_gt = file.readlines()
+    with open('rosbag_time.txt', 'r') as file:
+        rostime = file.readlines()
 
-    with open('sc1_robot2_gt.txt', 'r') as file:
-        robot2_gt = file.readlines()
+    bag_lin_vel = []
+    bag_ang_vel = []
 
-    esti_x = []
-    esti_y = []
+    gt_lin_vel = []
+    gt_ang_vel = []
+
+    bag_v_j = []
+    bag_w_j = []
+
     rosgbag_time = []
 
-    gt_robot1_x = []
-    gt_robot1_y = []
 
-    gt_robot2_x = []
-    gt_robot2_y = []
 
-    _esti_x = []
-    _esti_y = []
+    for line in lin_vel_r2:
+        gt_lin_vel.append(float(line.strip())) #gt
+        
 
-    thetaI = []
+    for line in ang_vel_r2:
+        gt_ang_vel.append(abs(float(line.strip()))) #gt
 
-    # for line in rostime:
-    #     rosgbag_time.append(float(line.strip()))
+    for line in bag_vj:
+        bag_v_j.append(float(line.strip()))
 
-    for line in theta_i:
-        thetaI.append(float(line.strip()))
+    for line in bag_wj:
+        bag_w_j.append(float(line.strip()))
 
-    for line in rosbag_x:
-        esti_x.append(float(line.strip()))
-        _esti_x.append(0)
 
-    for line in rosbag_y:
-        esti_y.append(float(line.strip()))
-        _esti_y.append(0)
-    downsample_ratio = int(9535 / 1127)
-    cnt = 0
 
-    for line in robot1_gt:
-        if cnt % downsample_ratio == 0:
-            x, y = map(float, line.strip().split('\t'))
-            gt_robot1_x.append(x)
-            gt_robot1_y.append(y)
-        cnt += 1
-    cnt = 0 
-    for line in robot2_gt:
-        if cnt % downsample_ratio == 0:
-            x, y = map(float, line.strip().split('\t'))
-            gt_robot2_x.append(x)
-            gt_robot2_y.append(y)
-        cnt += 1    
-    # print(len(gt_robot1_x))
-    # print(len(esti_x))
-    min_length = min(len(gt_robot1_x), len(esti_x), len(esti_y))
-    # print(range(min_length))
+    for line in rostime:
+        rosgbag_time.append(float(line.strip()))
+    print(np.mean(gt_lin_vel)-np.mean(bag_v_j))
 
-    for i in range(min_length):
-        # theta_cnt = i * 2 / 1127 * np.pi
-        # print(esti_x[1],esti_y[1])
-        # print("---------------------")
 
-        _esti_x[i] = m.cos(thetaI[i]*np.pi/180+np.pi/2) * esti_x[i] - m.sin(thetaI[i]*np.pi/180+m.pi/2) * esti_y[i]  + gt_robot1_x[i] 
-        _esti_y[i] = m.sin(thetaI[i]*np.pi/180+np.pi/2) * esti_x[i] + m.cos(thetaI[i]*np.pi/180+m.pi/2) * esti_y[i] + gt_robot1_y[i]  
-        # if i == 1:
-            # print("0 degree")
-            # print(theta_cnt)
-            # print(theta_cnt+np.pi/2)
-            # print(np.cos(theta_cnt+np.pi/2))
-            # print(init_x)
-            # print(np.sin(theta_cnt+np.pi/2))
-            # print(esti_y[i])
-            # print(gt_robot1_x[i])
-            # print(theta_cnt)
-            # print(np.cos(theta_cnt+np.pi/2),np.sin(theta_cnt+np.pi/2))
-        #     print(gt_robot1_x[i], gt_robot1_y[i])
-        #     print(_esti_x[i],_esti_y[i])
-        #     print("---------------------")
-        # elif i == 1128/4:
-        #     print("90 degree")
-        #     print(theta_cnt)
-        #     print(np.cos(theta_cnt+np.pi/2),np.sin(theta_cnt+np.pi/2))
-        #     print(gt_robot1_x[i], gt_robot1_y[i])
-        #     print(_esti_x[i],_esti_y[i])
-        #     print("---------------------")
-        # elif i == 1126/2:
-        #     print("180 degree")
-        #     print(theta_cnt)
-        #     print(np.cos(theta_cnt+np.pi/2),np.sin(theta_cnt+np.pi/2))
-        #     print(gt_robot1_x[i], gt_robot1_y[i])
-        #     print(_esti_x[i],_esti_y[i])
-        #     print("---------------------")
-        # elif i == 1128*3/4:
-        #     print("270 degree")
-        #     print(theta_cnt)
-        #     print(np.cos(theta_cnt+np.pi/2),np.sin(theta_cnt+np.pi/2))
-        #     print(gt_robot1_x[i], gt_robot1_y[i])
-        #     print(_esti_x[i],_esti_y[i])
     color = 'tab:red'
-    plt.plot(_esti_x, _esti_y, marker='s', linestyle='-', color=color, label='Estimated')
+    plt.plot(rosgbag_time, bag_v_j, marker='s', linestyle='-', color=color, label='estimation of linear velocity')
 
     color = 'tab:blue'
-    plt.plot(gt_robot2_x, gt_robot2_y, marker='s', linestyle='-', color=color, label='gt_r2')
+    plt.plot(rosgbag_time, gt_lin_vel, marker='s', linestyle='-', color=color, label='GT of linear velocity')
 
-    color = 'tab:green'
-    plt.plot(gt_robot1_x, gt_robot1_y, marker='s', linestyle='-', color=color, label='gt_r1')
+    # color = 'tab:green'
+    # plt.plot(gt_robot1_x, gt_robot1_y, marker='s', linestyle='-', color=color, label='gt_r1')
 
     plt.title('Estimation')
-    plt.xlabel('axis X')
-    plt.ylabel('axis Y')
+    plt.xlabel('Time')
+    plt.ylabel('m/s')
     plt.grid(True)
     plt.legend()
 
     plt.show()
 
 
-
 if __name__ =="__main__":
-    EKF()
+    linearplot()
